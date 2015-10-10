853a7ce9a45aba082ef4909767443bf91e159dd6
Michael <Mc_Smoove@hotmail.it>
2015-10-10 21:07:53 +0200

    Sync

--- src/checkMate/Test.java
+++ src/checkMate/Test.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package checkMate;
 import controller.*;
 import model.*;
@@ -21,7 +16,7 @@ public class Test {
         // TODO code application logic her
         
         ThreatBoard board = new ThreatBoard();
-        board.setBoard(m);
+      //  board.setBoard(m);
         
         
         
